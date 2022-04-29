338675a220615fe1b921dcb43d8ee1a3750bee38
verajiang <verajiang@tencent.com>
2022-04-29 11:52:20 +0800

    add double pointer

--- doublepointer/doublepointer.go
+++ doublepointer/doublepointer.go
@@ -1,7 +1,5 @@
 package doublepointer
 
-import "fmt"
-
 // https://leetcode-cn.com/problems/squares-of-a-sorted-array/
 func sortedSquares(nums []int) []int {
 	// diff left and right, get max to tag, then move left and right and tag
@@ -31,5 +29,36 @@ func sortedSquares(nums []int) []int {
 
 // https://leetcode-cn.com/problems/rotate-array/
 func rotate(nums []int, k int) {
-	fmt.Println("todo")
+	// 输入: nums = [1,2,3,4,5,6,7], k = 3
+	// 输出: [5,6,7,1,2,3,4]
+	// 解释:
+	// 向右轮转 1 步: [7,1,2,3,4,5,6]
+	// 向右轮转 2 步: [6,7,1,2,3,4,5]
+	// 向右轮转 3 步: [5,6,7,1,2,3,4]
+	rotate2(nums, k)
+}
+
+// T(n)=O(n)  S(n)=O(n)
+func rotate1(nums []int, k int) {
+	n := len(nums)
+	temp := make([]int, n)
+	for i := 0; i < n; i++ {
+		idx := (i + k) % n
+		temp[idx] = nums[i]
+	}
+	copy(nums, temp)
+}
+
+// T(n)=O(n) S(n)=O(1)
+func rotate2(nums []int, k int) {
+	k %= len(nums)
+	reverse(nums)
+	reverse(nums[:k])
+	reverse(nums[k:])
+}
+
+func reverse(a []int) {
+	for i, n := 0, len(a); i < n/2; i++ {
+		a[i], a[n-1-i] = a[n-1-i], a[i]
+	}
 }
--- doublepointer/doublepointer_test.go
+++ doublepointer/doublepointer_test.go
@@ -4,6 +4,21 @@ import (
 	"testing"
 )
 
+func IsEqual(a, b []int) bool {
+	lena := len(a)
+	lenb := len(b)
+	if lena != lenb {
+		return false
+	}
+
+	for i := 0; i < lena; i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
 // go test -v -run TestSortedSquares
 func TestSortedSquares(t *testing.T) {
 	var tests = []struct {
@@ -22,17 +37,21 @@ func TestSortedSquares(t *testing.T) {
 	}
 }
 
-func IsEqual(a, b []int) bool {
-	lena := len(a)
-	lenb := len(b)
-	if lena != lenb {
-		return false
+// go test -v -run TestRotate
+func TestRotate(t *testing.T) {
+	var tests = []struct {
+		nums []int
+		k    int
+		want []int
+	}{
+		{nums: []int{1, 2, 3, 4, 5, 6, 7}, k: 3, want: []int{5, 6, 7, 1, 2, 3, 4}},
 	}
 
-	for i := 0; i < lena; i++ {
-		if a[i] != b[i] {
-			return false
+	for _, v := range tests {
+		rotate(v.nums, v.k)
+		got := v.nums
+		if !IsEqual(got, v.want) {
+			t.Errorf("rotate(%v,%v) got: %v,wnat: %v", v.nums, v.k, got, v.want)
 		}
 	}
-	return true
 }
